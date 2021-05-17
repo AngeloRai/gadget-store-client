8de36cd60d736799116d96af7b785259da5193e2
Angelo Raimondi <angraimondi@gmail.com>
2021-05-16 21:54:41 -0300

    address added to index apis

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
   development: "http://localhost:4000",
-  production: "https://ironbeers-store.herokuapp.com",
+  production: "https://prime-gadgets.herokuapp.com/",
 };
 const api = axios.create({
   baseURL: apis[process.env.NODE_ENV],
